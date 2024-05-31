624c0cb25fd015d218fecba62fdf97f7d2d7b949
devartes <anacavalcante731@gmail.com>
2024-05-31 16:51:51 -0300

    refactor: fix styles and add code in constants.tsx

--- components/ui/MosaicsDouble.tsx
+++ components/ui/MosaicsDouble.tsx
@@ -1,135 +1,190 @@
-import type { ImageWidget } from "apps/admin/widgets.ts";
+import type { ImageWidget, VideoWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Video from "apps/website/components/Video.tsx";
-import type { VideoWidget } from "apps/admin/widgets.ts";
 
-export interface contentText {
+export interface Text {
   title?: string;
   description?: string;
 }
 
-export interface contentImage {
+export interface Image {
+  link?: string;
   /** @description title image */
   title?: string;
+  /** @description for better performance, if you want to use a GIF, prefer to use video */
   src?: ImageWidget;
   alt?: string;
+  content?: BannerContent;
 }
 
-export interface contentVideo {
+export interface Video {
   link?: string;
   url?: VideoWidget;
-  width?: number;
-  height?: number;
   description?: string;
+  content?: BannerContent;
 }
 
-export interface contentCta {
+export interface Cta {
   label?: string;
   href?: string;
 }
 
-export interface contentLink {
-  firstCta?: contentCta;
-  secondCta?: contentCta;
-  thirdCta?: contentCta;
-  fourthCta?: contentCta;
+export interface Link {
+  firstCta?: Cta;
+  secondCta?: Cta;
+  thirdCta?: Cta;
+  fourthCta?: Cta;
 }
 
-
-export interface contentBanner {
-  text?: contentText;
-  image?: contentImage;
-  video?: contentVideo;
-  link?: contentLink;
+export interface BannerContent {
+  text?: Text;
+  link?: Link;
 }
 
-export interface MosaicsDoubleProps {
-  firstBanner?: contentBanner;
-  secondBanner?: contentBanner;
+export interface MosaicsProps {
+  right?: Image | Video;
+  left?: Image | Video;
 }
 
-export default function MosaicsDouble(props: MosaicsDoubleProps) {
-  const { firstBanner, secondBanner } = props;
-
-  const renderBanner = (banner?: contentBanner) => {
-    if (!banner) return null;
-    const { image, video, link, text } = banner;
-
-    return (
-      <div class="flex flex-col items-center justify-center gap-[10px]">
-        {image && !video && (
-          <div class="relative m-[5px]">
-            <span class="absolute mosaic-bg top-0 left-0 w-full h-full z-10"></span>
-            <Image
-              src={image.src || ""}
-              alt={image.alt}
-              title={image.title}
-              width={705}
-              height={500}
-            />
-          </div>
-        )}
-        {video && !image && (
-          <Video
-            width={video.width || 900}
-            height={video.height || 773}
-            muted
-            autoPlay
-            controls
-            src={video.url || ""}
-            alt={video.description}
-            loading="lazy"
-            class="w-full h-full object-cover pt-20 pb-[60px]"
-          />
-        )}
-        {text && (
-          <div class="flex flex-col items-center justify-center absolute z-20">
-            <h2 class="text-2xl text-base-200">{text.title}</h2>
-            <p class="text-base-200 text-center">{text.description}</p>
-          </div>
-        )}
-        <div class="flex items-center justify-center absolute z-20 gap-[18px] mt-[100px]">
-          {link?.firstCta && (
-            <a
-              href={link.firstCta.href}
-              class="text-base-200"
-            >
-              {link.firstCta.label}
-            </a>
-          )}
-          {link?.secondCta && (
-            <a
-              href={link.secondCta.href}
-              class="text-base-200"
-            >
-              {link.secondCta.label}
-            </a>
-          )}
-          {link?.thirdCta && (
-            <a
-              href={link.thirdCta.href}
-              class="text-base-200"
-            >
-              {link.thirdCta.label}
-            </a>
-          )}
-          {link?.fourthCta && (
-            <a
-              href={link.fourthCta.href}
-              class="text-base-200"
-            >
-              {link.fourthCta.label}
-            </a>
-          )}
-        </div>
-      </div>
-    );
-  };
-
+export default function Mosaics({ right, left }: MosaicsProps) {
   return (
-    <div class="flex flex-col md:flex-row items-center justify-center md:px-[5px] px-2.5">
-      {renderBanner(firstBanner)}
-      {renderBanner(secondBanner)}
+    <div class="flex md:flex-row flex-col mx-auto items-center justify-center px-[5px]">
+      <div class="flex flex-col items-center justify-center gap-[10px]">
+        {left && (
+          <>
+            {"src" in left && (
+              <div class="relative m-[5px]">
+                <a href={left.link || ""} aria-label={left.title || ""}>
+                  <span class="absolute mosaic-bg top-0 left-0 w-full h-full z-10">
+                  </span>
+                  <Image
+                    src={left.src || ""}
+                    alt={left.alt || ""}
+                    title={left.title || ""}
+                    width={705}
+                    height={500}
+                    class="aspect-[705/500] object-cover"
+                  />
+                </a>
+              </div>
+            )}
+            {"url" in left && (
+              <div class="relative m-[5px]">
+                <a href={left.link || ""} aria-label={left.description || ""}>
+                  <Video
+                    width={705}
+                    height={500}
+                    muted
+                    autoPlay
+                    loop
+                    src={left.url || ""}
+                    alt={left.description || ""}
+                    loading="lazy"
+                    class="aspect-[705/500] object-cover"
+                  />
+                </a>
+                <span class="absolute mosaic-bg top-0 left-0 w-full h-full z-10">
+                </span>
+              </div>
+            )}
+            {left.content?.text && (
+              <div class="flex flex-col items-center justify-center absolute z-20">
+                <h2 class="text-2xl text-base-200">
+                  {left.content.text?.title}
+                </h2>
+                <p class="text-base-200 text-center">
+                  {left.content.text?.description}
+                </p>
+              </div>
+            )}
+            {left.content?.link && (
+              <div class="flex items-center justify-center absolute z-20 gap-[18px] mt-[100px]">
+                {left.content.link.firstCta &&
+                  renderCtaLink(left.content.link.firstCta)}
+                {left.content.link.secondCta &&
+                  renderCtaLink(left.content.link.secondCta)}
+                {left.content.link.thirdCta &&
+                  renderCtaLink(left.content.link.thirdCta)}
+                {left.content.link.fourthCta &&
+                  renderCtaLink(left.content.link.fourthCta)}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+      <div class="flex flex-col items-center justify-center gap-[10px]">
+        {right && (
+          <>
+            {"src" in right && (
+              <div class="relative m-[5px]">
+                <a href={right.link || ""} aria-label={right.title || ""}>
+                  <span class="absolute mosaic-bg top-0 left-0 w-full h-full z-10">
+                  </span>
+                  <Image
+                    src={right.src || ""}
+                    alt={right.alt || ""}
+                    title={right.title || ""}
+                    width={705}
+                    height={500}
+                    class="aspect-[705/500] object-cover"
+                  />
+                </a>
+              </div>
+            )}
+            {"url" in right && (
+              <div class="relative m-[5px]">
+                <a href={right.link || ""} aria-label={right.description || ""}>
+                  <Video
+                    width={705}
+                    height={500}
+                    muted
+                    autoPlay
+                    loop
+                    src={right.url || ""}
+                    alt={right.description || ""}
+                    loading="lazy"
+                    class="aspect-[705/500] object-cover"
+                  />
+                </a>
+                <span class="absolute mosaic-bg top-0 left-0 w-full h-full z-10">
+                </span>
+              </div>
+            )}
+            {right.content?.text && (
+              <div class="flex flex-col items-center justify-center absolute z-20">
+                <h2 class="text-2xl text-base-200">
+                  {right.content.text?.title}
+                </h2>
+                <p class="text-base-200 text-center">
+                  {right.content.text?.description}
+                </p>
+              </div>
+            )}
+            {right.content?.link && (
+              <div class="flex items-center justify-center absolute z-20 gap-[18px] mt-[100px]">
+                {right.content.link.firstCta &&
+                  renderCtaLink(right.content.link.firstCta)}
+                {right.content.link.secondCta &&
+                  renderCtaLink(right.content.link.secondCta)}
+                {right.content.link.thirdCta &&
+                  renderCtaLink(right.content.link.thirdCta)}
+                {right.content.link.fourthCta &&
+                  renderCtaLink(right.content.link.fourthCta)}
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
+  );
+}
+
+function renderCtaLink(cta: Cta) {
+  return (
+    <a href={cta.href} class="text-base-200 group" aria-label={cta.label}>
+      {cta.label}
+      <span class="block h-0.5 bg-base-200 w-full transition-transform duration-300 transform group-hover:translate-y-[-2px] h-[0.5px]">
+      </span>
+    </a>
   );
 }
